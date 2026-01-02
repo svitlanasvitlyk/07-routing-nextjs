179ad732b4495046239d75b102f9a5a6fb53c125
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2026-01-02 19:34:47 +0100

    update header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,29 +1,23 @@
 import css from './Header.module.css';
-import Link from 'next/link';
-import { getTags } from '@/lib/api';
-import TagsMenu from '../TagsMenu/TagsMenu';
 
-const Header = () => {
-  const tags = getTags();
+import Link from 'next/link';
+
+export default function Header() {
   return (
     <header className={css.header}>
-      <Link className="headerLink" href="/" aria-label="Home">
+      <Link href="/" aria-label="Home">
         NoteHub
       </Link>
-      <nav className="navigation" aria-label="Main Navigation">
+      <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
-          <li className="navigationItem">
-            <Link className="navigationLink" href="/">
-              Home
-            </Link>
+          <li>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <TagsMenu tags={tags} />
+            <Link href="/notes/filter/all">Notes</Link>
           </li>
         </ul>
       </nav>
     </header>
   );
-};
-
-export default Header;
+}
